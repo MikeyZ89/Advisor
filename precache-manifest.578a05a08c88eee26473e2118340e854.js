e61e6d7c141dfff873b90a90777663ea955f9ceb
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-12-09 11:46:32 -0700

    Updates

---
+++ precache-manifest.578a05a08c88eee26473e2118340e854.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3498430d40eb607ca901ec92f7c19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bdc85c282edd889c56e",
+    "url": "/static/js/2.0372a2b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0372a2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360b0e2e86392858678c",
+    "url": "/static/js/main.9da7b9cb.chunk.js"
+  },
+  {
+    "revision": "b7383c0dce46a1611bcf",
+    "url": "/static/js/runtime-main.625f24a9.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/static/media/svg-3.397c455a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/advisor_Site/precache-manifest.3df6f7dd7116f14160b87e04b75271db.js"
+  "/precache-manifest.578a05a08c88eee26473e2118340e854.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/advisor_Site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
